e866850fcf3a0c0e7e15ddc747bdc2c29b7c4ece
Vitalii Ovechkin <ovechkinve@yandex-team.ru>
2020-11-15 00:36:14 +0300

    Make module hierarchy

--- src/third_party/io_classification/run_placesCNN_unified.py
+++ src/third_party/io_classification/run_placesCNN_unified.py
@@ -4,7 +4,6 @@
 
 import torch
 from torch.autograd import Variable as V
-import torchvision.models as models
 from torchvision import transforms as trn
 from torch.nn import functional as F
 import os
@@ -37,7 +36,7 @@ def load_labels():
         labels_IO = []
         for line in lines:
             items = line.rstrip().split()
-            labels_IO.append(int(items[-1]) -1) # 0 is indoor, 1 is outdoor
+            labels_IO.append(int(items[-1]) - 1)  # 0 is indoor, 1 is outdoor
     labels_IO = np.array(labels_IO)
 
     # scene attribute relevant
@@ -56,8 +55,10 @@ def load_labels():
 
     return classes, labels_IO, labels_attribute, W_attribute
 
+
 def hook_feature(module, input, output):
     features_blobs.append(np.squeeze(output.data.cpu().numpy()))
+
 
 def returnCAM(feature_conv, weight_softmax, class_idx):
     # generate the class activation maps upsample to 256x256
@@ -65,7 +66,7 @@ def returnCAM(feature_conv, weight_softmax, class_idx):
     nc, h, w = feature_conv.shape
     output_cam = []
     for idx in class_idx:
-        cam = weight_softmax[class_idx].dot(feature_conv.reshape((nc, h*w)))
+        cam = weight_softmax[class_idx].dot(feature_conv.reshape((nc, h * w)))
         cam = cam.reshape(h, w)
         cam = cam - np.min(cam)
         cam_img = cam / np.max(cam)
@@ -73,10 +74,11 @@ def returnCAM(feature_conv, weight_softmax, class_idx):
         output_cam.append(imresize(cam_img, size_upsample))
     return output_cam
 
+
 def returnTF():
-# load the image transformer
+    # load the image transformer
     tf = trn.Compose([
-        trn.Resize((224,224)),
+        trn.Resize((224, 224)),
         trn.ToTensor(),
         trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
@@ -94,20 +96,20 @@ def load_model():
     if useGPU == 1:
         model = torch.load(model_file)
     else:
-        model = torch.load(model_file, map_location=lambda storage, loc: storage) # allow cpu
+        model = torch.load(model_file, map_location=lambda storage, loc: storage)  # allow cpu
 
     # the following is deprecated, everything is migrated to python36
 
     ## if you encounter the UnicodeDecodeError when use python3 to load the model, add the following line will fix it. Thanks to @soravux
-    #from functools import partial
-    #import pickle
-    #pickle.load = partial(pickle.load, encoding="latin1")
-    #pickle.Unpickler = partial(pickle.Unpickler, encoding="latin1")
-    #model = torch.load(model_file, map_location=lambda storage, loc: storage, pickle_module=pickle)
+    # from functools import partial
+    # import pickle
+    # pickle.load = partial(pickle.load, encoding="latin1")
+    # pickle.Unpickler = partial(pickle.Unpickler, encoding="latin1")
+    # model = torch.load(model_file, map_location=lambda storage, loc: storage, pickle_module=pickle)
 
     model.eval()
     # hook the feature extractor
-    features_names = ['layer4','avgpool'] # this is the last conv layer of the resnet
+    features_names = ['layer4', 'avgpool']  # this is the last conv layer of the resnet
     for name in features_names:
         model._modules.get(name).register_forward_hook(hook_feature)
     return model
@@ -121,12 +123,12 @@ features_blobs = []
 model = load_model()
 
 # load the transformer
-tf = returnTF() # image transformer
+tf = returnTF()  # image transformer
 
 # get the softmax weight
 params = list(model.parameters())
 weight_softmax = params[-2].data.numpy()
-weight_softmax[weight_softmax<0] = 0
+weight_softmax[weight_softmax < 0] = 0
 
 # load the test image
 # img_url = 'http://places2.csail.mit.edu/imgs/12.jpg'
@@ -143,7 +145,7 @@ probs, idx = h_x.sort(0, True)
 print('RESULT ON ' + img_url)
 
 # output the IO prediction
-io_image = np.mean(labels_IO[idx[:10].numpy()]) # vote for the indoor or outdoor
+io_image = np.mean(labels_IO[idx[:10].numpy()])  # vote for the indoor or outdoor
 if io_image < 0.5:
     print('--TYPE OF ENVIRONMENT: indoor')
 else:
@@ -158,8 +160,7 @@ for i in range(0, 5):
 responses_attribute = W_attribute.dot(features_blobs[1])
 idx_a = np.argsort(responses_attribute)
 print('--SCENE ATTRIBUTES:')
-print(', '.join([labels_attribute[idx_a[i]] for i in range(-1,-10,-1)]))
-
+print(', '.join([labels_attribute[idx_a[i]] for i in range(-1, -10, -1)]))
 
 # generate class activation mapping
 print('Class activation map is saved as cam.jpg')
@@ -168,6 +169,6 @@ CAMs = returnCAM(features_blobs[0], weight_softmax, [idx[0]])
 # render the CAM and output
 img = cv2.imread('test.jpg')
 height, width, _ = img.shape
-heatmap = cv2.applyColorMap(cv2.resize(CAMs[0],(width, height)), cv2.COLORMAP_JET)
+heatmap = cv2.applyColorMap(cv2.resize(CAMs[0], (width, height)), cv2.COLORMAP_JET)
 result = heatmap * 0.4 + img * 0.5
 cv2.imwrite('cam.jpg', result)
